8b23b8ee7c193e0ea1a7f1bfc2619f8e40728646
tmcgroarty <timothysmcgroarty@gmail.com>
2026-02-16 16:36:11 -0500

    Refactor variables in variables.tf for clarity

--- steelhead/variables.tf
+++ steelhead/variables.tf
@@ -1,29 +1,40 @@
-variable "vm_name" { type = string }
-variable "admin_username" { type = string }
+variable "name_prefix" {
+  type        = string
+  description = "Short prefix for names (e.g., tim)"
+}
 
-# Set via env var (interactive prompt in shell):
-#   export TF_VAR_admin_password="..."
+variable "vm_base_name" {
+  type        = string
+  description = "Base name (e.g., sh01)"
+}
+
+variable "location" {
+  type = string
+}
+
+variable "admin_username" {
+  type = string
+}
+
+# Set via env var: TF_VAR_admin_password
 variable "admin_password" {
   type      = string
   sensitive = true
 }
 
-variable "rg_name"   { type = string }
-variable "location"  { type = string }
-
-variable "vnet_name" { type = string }
-variable "vnet_address_space" {
-  type = list(string)
-}
-
-variable "subnet_name" { type = string }
-variable "subnet_prefixes" {
-  type = list(string)
-}
-
 variable "vm_size" {
   type    = string
   default = "Standard_D4s_v5"
+}
+
+variable "vnet_address_space" {
+  type    = list(string)
+  default = ["10.50.0.0/16"]
+}
+
+variable "subnet_prefixes" {
+  type    = list(string)
+  default = ["10.50.1.0/24"]
 }
 
 variable "data_disk_size_gb" {
